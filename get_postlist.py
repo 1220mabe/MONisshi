7cd244bf71d56ce813a6e71182aaefab3f4d1e1f
1220mabe <53966112+1220mabe@users.noreply.github.com>
2020-07-27 23:33:02 +0900

    Add files via upload

---
+++ get_postlist.py
@@ -0,0 +1,146 @@
+import os
+import requests
+import csv
+import re
+import post_article
+import output_graph
+from datetime import datetime, date, timedelta
+from bs4 import BeautifulSoup
+from urllib.request import urlopen
+from collections import Counter
+from collections import OrderedDict
+from datetime import date,timedelta
+
+three_weeks_ago = datetime.today() + timedelta(weeks=-3)
+two_weeks_ago = datetime.today() + timedelta(weeks=-2)
+one_weeks_ago = datetime.today() + timedelta(weeks=-1)
+today_str = datetime.today().strftime('%Y/%m/%d')
+
+def get_graph_stan(weeks_ago):
+    metadeck = []
+    for i in range(50):
+        #URL Pageループ
+        url ="https://teamqno.work/category/decks/standard-metagame/page/{}".format(i+1)
+        #print(url)
+        r = requests.get(url)
+        soup = BeautifulSoup(r.content, "html.parser")
+        decks = soup.findAll('div', class_='entry-card-content card-content e-card-content')
+        #デッキループ
+        for deck in decks:
+            deck_name = deck.find('h2', class_='entry-card-title card-title e-card-title')
+            deck_post_date = deck.find("span",{"class":"post-date"})
+            # 日付比較
+            # 基準日の1週間前を算出
+            date_str=deck_post_date.get_text()
+            #print(date_str)
+            tdatetime = datetime.strptime(date_str, ' %Y.%m.%d')
+
+            #print(one_weeks_ago<=tdatetime)
+            if weeks_ago<=tdatetime:
+                metadeck.append(deck_name.get_text())
+
+
+    #グラフ出力
+    mycounter = Counter(metadeck)
+    index, data = zip(*mycounter.most_common())
+    #print(list(index))
+    #print(list(data))
+    ns = map(lambda s: int(s), list(data))
+    tindex = list(index)
+    tdata = list(ns)
+    tindex.reverse()
+    tdata.reverse()
+    #print(tindex)
+    #print(tdata)
+    xlabel = "Standard Metagame " + weeks_ago.strftime('%Y/%m/%d')  + " - "  + today_str + " by Team Qno Research"
+    #print(xlabel)
+    output_graph.make_graph(tindex,tdata,xlabel)
+    return
+
+def get_graph_hist(weeks_ago):
+    metadeck = []
+    for i in range(50):
+        #URL Pageループ
+        url ="https://teamqno.work/category/decks/historic-metagame//page/{}".format(i+1)
+        #print(url)
+        r = requests.get(url)
+        soup = BeautifulSoup(r.content, "html.parser")
+        decks = soup.findAll('div', class_='entry-card-content card-content e-card-content')
+        #デッキループ
+        for deck in decks:
+            deck_name = deck.find('h2', class_='entry-card-title card-title e-card-title')
+            deck_post_date = deck.find("span",{"class":"post-date"})
+            # 日付比較
+            date_str=deck_post_date.get_text()
+            #print(date_str)
+            tdatetime = datetime.strptime(date_str, ' %Y.%m.%d')
+
+            #print(one_weeks_ago<=tdatetime)
+            if weeks_ago<=tdatetime:
+                metadeck.append(deck_name.get_text())
+
+
+    #グラフ出力
+    mycounter = Counter(metadeck)
+    index, data = zip(*mycounter.most_common())
+    #print(list(index))
+    #print(list(data))
+    ns = map(lambda s: int(s), list(data))
+    tindex = list(index)
+    tdata = list(ns)
+    tindex.reverse()
+    tdata.reverse()
+    #print(tindex)
+    #print(tdata)
+    xlabel = "Historic Metagame " + weeks_ago.strftime('%Y/%m/%d') + " - "  + today_str + " by Team Qno Research"
+    #print(xlabel)
+    output_graph.make_graph(tindex,tdata,xlabel)
+    return
+
+def get_graph_pion(weeks_ago):
+    metadeck = []
+    for i in range(50):
+        #URL Pageループ
+        url ="https://teamqno.work/category/decks/pioneer-metagame//page/{}".format(i+1)
+        #print(url)
+        r = requests.get(url)
+        soup = BeautifulSoup(r.content, "html.parser")
+        decks = soup.findAll('div', class_='entry-card-content card-content e-card-content')
+        #デッキループ
+        for deck in decks:
+            deck_name = deck.find('h2', class_='entry-card-title card-title e-card-title')
+            deck_post_date = deck.find("span",{"class":"post-date"})
+            # 日付比較
+            date_str=deck_post_date.get_text()
+            #print(date_str)
+            tdatetime = datetime.strptime(date_str, ' %Y.%m.%d')
+
+            #print(one_weeks_ago<=tdatetime)
+            if weeks_ago<=tdatetime:
+                metadeck.append(deck_name.get_text())
+
+
+    #グラフ出力
+    mycounter = Counter(metadeck)
+    index, data = zip(*mycounter.most_common())
+    #print(list(index))
+    #print(list(data))
+    ns = map(lambda s: int(s), list(data))
+    tindex = list(index)
+    tdata = list(ns)
+    tindex.reverse()
+    tdata.reverse()
+    #print(tindex)
+    #print(tdata)
+    xlabel = "Pioneer Metagame " + weeks_ago.strftime('%Y/%m/%d') + " - "  + today_str + " by Team Qno Research"
+    #print(xlabel)
+    output_graph.make_graph(tindex,tdata,xlabel)
+    return
+
+
+# 実行
+get_graph_stan(one_weeks_ago)
+#get_graph_hist(one_weeks_ago)
+get_graph_pion(one_weeks_ago)
+# get_graph_pion(two_weeks_ago)
+# get_graph_pion(three_weeks_ago)
---
+++ main_decklist.py
@@ -0,0 +1,354 @@
+print("---------------Start getting MO List------------------")
+
+import os
+import requests
+import csv
+import re
+import categorize_decks
+import post_article
+import post_tweet
+from datetime import datetime, date, timedelta
+from bs4 import BeautifulSoup
+from urllib.request import urlopen
+
+today =  datetime.today()
+yesterday = today - timedelta(days=1)
+dbyesterday = today - timedelta(days=2)
+tdyesterday = today - timedelta(days=3)
+
+mo = "https://magic.wizards.com/en/articles/archive/mtgo-standings/"
+stdlea = "Standard-League"
+stdcha = "Standard-Challenge"
+stdpre = "Standard-Preliminary"
+piolea = "Pioneer-League"
+piocha = "Pioneer-Challenge"
+piopre = "Pioneer-Preliminary"
+categorystan = "Decks,Standard"
+categorypio = "Decks,Pioneer"
+
+csvfile = ".csv"
+txtfile = ".txt"
+DeckPath = "E:\\TeamQno\\Decks\\"
+
+def make_row(span_row_list, decks):
+    for span_row in span_row_list:
+        row = []
+        card_count_r = span_row.find("span",{"class":"card-count"})
+        card_name_r = span_row.find("span",{"class":"card-name"})
+        card_count = str(card_count_r.text)
+        card_name = str(card_name_r.text)
+        row.append("<span style=\"font-size: 14px;\">" + card_count)
+        row.append(card_name + "</span>")
+        row_str=' '.join(row)
+        decks.append(row_str)
+    return
+
+def make_row_simple(span_row_list, decks):
+    for span_row in span_row_list:
+        row = []
+        card_count_r = span_row.find("span",{"class":"card-count"})
+        card_name_r = span_row.find("span",{"class":"card-name"})
+        card_count = str(card_count_r.text)
+        card_name = str(card_name_r.text)
+        row.append("\n" + card_count)
+        row.append(card_name)
+        row_str=' '.join(row)
+        decks.append(row_str)
+    return
+
+#MOデッキリストをcsvファイル化
+def to_csv_mo(url, filename,filename_,title,category):
+    TweetUrl = ""
+    csvheader_data = []
+    # ファイルヘッダー挿入
+    csvheader_data.append('post_id')
+    csvheader_data.append('post_name')
+    csvheader_data.append('post_author')
+    csvheader_data.append('post_date')
+    csvheader_data.append('post_type')
+    csvheader_data.append('post_status')
+    csvheader_data.append('post_thumbnail')
+    csvheader_data.append('post_title')
+    csvheader_data.append('post_content')
+    csvheader_data.append('post_category')
+    csvheader_data.append('post_tags')
+    csvheader_data.append('custom_field')
+    csvheader_data.append('custom_field')
+    csvheader_data.append('post_thumbnail')
+
+    r = requests.get(url)
+    soup = BeautifulSoup(r.content, "html.parser")
+    players = soup.select('.content.beanSpacing')
+
+    for player in players:
+        final_csv_data = []
+        csv_data=[]
+        decks = []
+        decks_arena = []
+        player_name = player.find("h4")
+        # ファイル存在チェック
+        if os.path.exists(filename + " " + player_name.text + csvfile) and os.path.getsize(filename + " " + player_name.text + csvfile) > 0:
+            return
+        # ファイルオープン
+        csv_file = open(filename + " " + player_name.text + csvfile, 'wt', newline = '', encoding = 'utf-8')
+        csv_write = csv.writer(csv_file)
+        decks.append("<center>")
+        decks.append("<b>" + title +"</b>")
+        decks.append("<b>" + player_name.text + "</b>")
+        decks.append("[mtg_deck]")
+        decks_arena.append("Deck")
+        #プレインズウォーカー
+        planeswalkers = player.find('div', class_='sorted-by-planeswalker clearfix element')
+        if planeswalkers:
+            decks.append("<span style=\"font-size: 14px;\"><b>Planeswalker</b></span>")
+            span_row_list = planeswalkers.findAll("span",{"class":"row"})
+            make_row(span_row_list, decks)
+            make_row_simple(span_row_list, decks_arena)
+        #クリーチャー
+        creatures = player.find('div', class_='sorted-by-creature clearfix element')
+        if creatures:
+            decks.append("<span style=\"font-size: 14px;\"><b>Creature</b></span>")
+            span_row_list = creatures.findAll("span",{"class":"row"})
+            make_row(span_row_list, decks)
+            make_row_simple(span_row_list, decks_arena)
+        #インスタント
+        instants = player.find('div', class_='sorted-by-instant clearfix element')
+        if instants:
+            decks.append("<span style=\"font-size: 14px;\"><b>Instant</b></span>")
+            span_row_list = instants.findAll("span",{"class":"row"})
+            make_row(span_row_list, decks)
+            make_row_simple(span_row_list, decks_arena)
+        #ソーサリー
+        sorcerys = player.find('div', class_='sorted-by-sorcery clearfix element')
+        if sorcerys:
+            decks.append("<span style=\"font-size: 14px;\"><b>Sorcery</b></span>")
+            span_row_list = sorcerys.findAll("span",{"class":"row"})
+            make_row(span_row_list, decks)
+            make_row_simple(span_row_list, decks_arena)
+        #エンチャント
+        enchantments = player.find('div', class_='sorted-by-enchantment clearfix element')
+        if enchantments:
+            decks.append("<span style=\"font-size: 14px;\"><b>Enchantment</b></span>")
+            span_row_list = enchantments.findAll("span",{"class":"row"})
+            make_row(span_row_list, decks)
+            make_row_simple(span_row_list, decks_arena)
+        #アーティファクト
+        artifacts = player.find('div', class_='sorted-by-artifact clearfix element')
+        if artifacts:
+            decks.append("<span style=\"font-size: 14px;\"><b>Artifact</b></span>")
+            span_row_list = artifacts.findAll("span",{"class":"row"})
+            make_row(span_row_list, decks)
+            make_row_simple(span_row_list, decks_arena)
+        #土地
+        lands = player.find('div', class_='sorted-by-land clearfix element')
+        if lands:
+            decks.append("<span style=\"font-size: 14px;\"><b>Land</b></span>")
+            span_row_list = lands.findAll("span",{"class":"row"})
+            make_row(span_row_list, decks)
+            make_row_simple(span_row_list, decks_arena)
+        #サイドボード
+        sideboards = player.find('div', class_='sorted-by-sideboard-container clearfix element')
+        if sideboards:
+            decks.append("<span style=\"font-size: 14px;\"><b>Sideboard</b></span>")
+            decks_arena.append("\n\n")
+            decks_arena.append("Sideboard")
+            span_row_list = sideboards.findAll("span",{"class":"row"})
+            make_row(span_row_list, decks)
+            make_row_simple(span_row_list, decks_arena)
+        decks.append("[/mtg_deck]")
+        # MTGACode Botton作成
+        today_str = datetime.today().strftime('%Y/%m/')
+        mtgacode_url = ("https://teamqno.work/wp-content/uploads/"+ today_str + filename_ + " " + player_name.text + txtfile).replace(' ', '-').replace('(', '').replace(')', '')
+        mtgacode_botton = ("[maxbutton id=\"9\" url=\"%s\"]"  % mtgacode_url)
+        decks.append(mtgacode_botton)
+        decks.append("</center>")
+
+        csv_data.append('\r\n'.join(decks))
+
+        #アーキタイプカテゴライズ
+        if category == categorystan:category_list = [15,34]
+        if category == categorystan: results = categorize_decks.check_arc_stan(csv_data)
+        if category == categorypio:category_list = [15,36]
+        if category == categorypio:results = categorize_decks.check_arc_pion(csv_data)
+        post_title = results[0]
+        media_id = results[1]
+        #タグID作成
+        post_article.post_tag(title)
+        #タグID取得
+        title_list = []
+        title_list.append(int(post_article.get_tags()))
+        #ポスト(投稿)
+        post_article.post_article('publish',post_title,post_title,'\r\n\r\n'.join(csv_data),category_list,title_list,media_id)
+
+        # csvファイル出力用
+        final_csv_data.append('')
+        final_csv_data.append('')
+        final_csv_data.append('qno')
+        final_csv_data.append('')
+        final_csv_data.append('')
+        final_csv_data.append('private')
+        final_csv_data.append('')
+        final_csv_data.append(post_title)
+        final_csv_data.append('\r\n\r\n'.join(csv_data))
+        final_csv_data.append(category)
+        final_csv_data.append(title)
+        final_csv_data.append('')
+        final_csv_data.append('')
+        csv_write.writerow(csvheader_data)
+        csv_write.writerow(final_csv_data)
+        # csvファイルクローズド
+        csv_file.close()
+        if os.path.getsize(filename + " " + player_name.text + csvfile) <= 0:
+            os.remove(filename + " " + player_name.text + csvfile)
+        # MTGAtxtファイル存在チェック
+        if os.path.exists(filename + " " + player_name.text + txtfile) and os.path.getsize(filename + " " + player_name.text + txtfile) > 0:
+            return
+        # MTGAtxtファイル追記
+        with open(filename + " " + player_name.text + txtfile, mode='w') as f:
+            f.writelines(decks_arena)
+        # MTGAtxtファイルクローズド
+        f.close()
+        # MTGAtxtファイルアップロード
+        post_article.post_txt(filename + " " + player_name.text + txtfile)
+        if os.path.getsize(filename + " " + player_name.text + txtfile) <= 0:
+            os.remove(filename + " " + player_name.text + txtfile)
+
+        TweetUrl = r"https://teamqno.work/tag/" + title
+    return TweetUrl
+
+
+
+# デッキリストポストとcsv作成
+#スタンダード
+title = stdlea + tdyesterday.strftime('-%Y-%m-%d')
+url = mo + title
+filename = DeckPath + stdlea + tdyesterday.strftime('-%Y-%m-%d')
+filename_ = stdlea + tdyesterday.strftime('-%Y-%m-%d')
+result_mes = to_csv_mo(url,filename,filename_,title,categorystan)
+
+
+title = stdlea + today.strftime('-%Y-%m-%d')
+url = mo + title
+filename = DeckPath + stdlea + today.strftime('-%Y-%m-%d')
+filename_ = stdlea + today.strftime('-%Y-%m-%d')
+result_mes = to_csv_mo(url,filename,filename_,title,categorystan)
+
+
+title = stdlea + yesterday.strftime('-%Y-%m-%d')
+url = mo + title
+filename = DeckPath +stdlea + yesterday.strftime('-%Y-%m-%d')
+filename_ = stdlea + yesterday.strftime('-%Y-%m-%d')
+result_mes = to_csv_mo(url,filename,filename_,title,categorystan)
+
+title = stdlea + dbyesterday.strftime('-%Y-%m-%d')
+url = mo + title
+filename = DeckPath +stdlea + dbyesterday.strftime('-%Y-%m-%d')
+filename_ =stdlea + dbyesterday.strftime('-%Y-%m-%d')
+result_mes = to_csv_mo(url,filename,filename_,title,categorystan)
+
+title = stdcha + today.strftime('-%Y-%m-%d')
+url = mo + title
+filename = DeckPath +stdcha + today.strftime('-%Y-%m-%d')
+filename_ = stdcha + today.strftime('-%Y-%m-%d')
+result_mes = to_csv_mo(url,filename,filename_,title,categorystan)
+
+
+title =  stdcha + yesterday.strftime('-%Y-%m-%d')
+url = mo + title
+filename = DeckPath +stdcha + yesterday.strftime('-%Y-%m-%d')
+filename_ = stdcha + yesterday.strftime('-%Y-%m-%d')
+result_mes = to_csv_mo(url,filename,filename_,title,categorystan)
+
+
+title =  stdcha + dbyesterday.strftime('-%Y-%m-%d')
+url = mo + title
+filename = DeckPath +stdcha + dbyesterday.strftime('-%Y-%m-%d')
+filename_ = stdcha + dbyesterday.strftime('-%Y-%m-%d')
+result_mes = to_csv_mo(url,filename,filename_,title,categorystan)
+
+
+title =  stdpre + today.strftime('-%Y-%m-%d')
+url = mo + title
+filename = DeckPath +stdpre + today.strftime('-%Y-%m-%d')
+filename_ = stdpre + today.strftime('-%Y-%m-%d')
+result_mes = to_csv_mo(url,filename,filename_,title,categorystan)
+
+
+title = stdpre + yesterday.strftime('-%Y-%m-%d')
+url = mo + title
+filename = DeckPath +stdpre + yesterday.strftime('-%Y-%m-%d')
+filename_ = stdpre + yesterday.strftime('-%Y-%m-%d')
+result_mes = to_csv_mo(url,filename,filename_,title,categorystan)
+
+
+title =  stdpre + today.strftime('-%Y-%m-%d')
+url = mo + title
+filename = DeckPath +stdpre + today.strftime('-%Y-%m-%d')
+filename_ = stdpre + today.strftime('-%Y-%m-%d')
+result_mes = to_csv_mo(url,filename,filename_,title,categorystan)
+
+
+title = stdpre + dbyesterday.strftime('-%Y-%m-%d')
+url = mo + title
+filename = DeckPath +stdpre + dbyesterday.strftime('-%Y-%m-%d')
+filename_ = stdpre + dbyesterday.strftime('-%Y-%m-%d')
+result_mes = to_csv_mo(url,filename,filename_,title,categorystan)
+
+#パイオニア
+title =  piolea + today.strftime('-%Y-%m-%d')
+url = mo + title
+filename = DeckPath +piolea + today.strftime('-%Y-%m-%d')
+filename_ = piolea + today.strftime('-%Y-%m-%d')
+result_mes = to_csv_mo(url,filename,filename_,title,categorypio)
+
+title =  piolea + yesterday.strftime('-%Y-%m-%d')
+url = mo + title
+filename = DeckPath +piolea + yesterday.strftime('-%Y-%m-%d')
+filename_ = piolea + yesterday.strftime('-%Y-%m-%d')
+result_mes = to_csv_mo(url,filename,filename_,title,categorypio)
+
+title =  piolea + dbyesterday.strftime('-%Y-%m-%d')
+url = mo + title
+filename = DeckPath +piolea + dbyesterday.strftime('-%Y-%m-%d')
+filename_ = piolea + dbyesterday.strftime('-%Y-%m-%d')
+result_mes = to_csv_mo(url,filename,filename_,title,categorypio)
+
+title =  piocha + today.strftime('-%Y-%m-%d')
+url = mo + title
+filename = DeckPath +piocha + today.strftime('-%Y-%m-%d')
+filename_ = piocha + today.strftime('-%Y-%m-%d')
+result_mes = to_csv_mo(url,filename,filename_,title,categorypio)
+
+title = piocha + yesterday.strftime('-%Y-%m-%d')
+url = mo + title
+filename = DeckPath +piocha + yesterday.strftime('-%Y-%m-%d')
+filename_ = piocha + yesterday.strftime('-%Y-%m-%d')
+result_mes = to_csv_mo(url,filename,filename_,title,categorypio)
+
+title = piocha + dbyesterday.strftime('-%Y-%m-%d')
+url = mo + title
+filename = DeckPath +piocha + dbyesterday.strftime('-%Y-%m-%d')
+filename_ = piocha + dbyesterday.strftime('-%Y-%m-%d')
+result_mes = to_csv_mo(url,filename,filename_,title,categorypio)
+
+
+title = piopre + today.strftime('-%Y-%m-%d')
+url = mo + title
+filename = DeckPath +piopre + today.strftime('-%Y-%m-%d')
+filename_ = piopre + today.strftime('-%Y-%m-%d')
+result_mes = to_csv_mo(url,filename,filename_,title,categorypio)
+
+
+title =  piopre + yesterday.strftime('-%Y-%m-%d')
+url = mo + title
+filename = DeckPath +piopre + yesterday.strftime('-%Y-%m-%d')
+filename_ = piopre + yesterday.strftime('-%Y-%m-%d')
+result_mes = to_csv_mo(url,filename,filename_,title,categorypio)
+
+title =  piopre + dbyesterday.strftime('-%Y-%m-%d')
+url = mo + title
+filename = DeckPath +piopre + dbyesterday.strftime('-%Y-%m-%d')
+filename_ = piopre + dbyesterday.strftime('-%Y-%m-%d')
+result_mes = to_csv_mo(url,filename,filename_,title,categorypio)
+
+print("---------------End getting MO List--------------------")
